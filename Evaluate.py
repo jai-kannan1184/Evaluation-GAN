7b504961262cb00a75580951b43bc075abba48f0
jai-kannan1184 <jai.kannan@deakin.edu.au>
2019-05-06 17:45:36 +1000

    ENHANCE: Resolved the resize of images for accurate ssim index

--- Evaluate.py
+++ Evaluate.py
@@ -30,25 +30,39 @@ def compare_image(imageA, imageB, title):
 #Load the images
 map_1= cv2.imread('images/509.jpg')
 map_2= cv2.imread('images/509actual.png')
+map_3= cv2.imread('images/508-outputs.png')
+map_4= cv2.imread('images/508-targets.png')
+map_5= cv2.imread('images/1.png')
+map_6= cv2.imread('images/1t.png')
 
 #Resize into zero patting
 map_1= cv2.resize(map_1, (10000, 10000))
 map_2= cv2.resize(map_2, (10000, 10000))
+map_3= cv2.resize(map_3, (10000, 10000))
+map_4= cv2.resize(map_4, (10000, 10000))
+#map_5= cv2.resize(map_5, (10000, 10000))
+#map_6= cv2.resize(map_6, (10000, 10000))
 
 #convert to gray
 map_1= cv2.cvtColor(map_1, cv2.COLOR_BGR2GRAY)
 map_2= cv2.cvtColor(map_2, cv2.COLOR_BGR2GRAY)
+map_3= cv2.cvtColor(map_3, cv2.COLOR_BGR2GRAY)
+map_4=cv2.cvtColor(map_4, cv2.COLOR_BGR2GRAY)
+map_5=cv2.cvtColor(map_5, cv2.COLOR_BGR2GRAY)
+map_6=cv2.cvtColor(map_6, cv2.COLOR_BGR2GRAY)
 
 #initilize the figure
 fig = plt.figure('maps')
-images = ('First map', map_1), ('Second map', map_2)
+images = ('First map', map_1), ('Second map', map_2), ('Third map', map_3), ('Fourth map', map_4), ('Fifth map', map_5), ('Sixith map', map_6)
 
 #loop the images
 for(i, (name, image)) in enumerate(images):
-    ax = fig.add_subplot(1, 2, i+1)
+    ax = fig.add_subplot(1, 6, i+1)
     #plt.imshow(images, cmap= plt.cm.gray)
     plt.axis('off')
 
 plt.show()
 compare_image(map_1,map_1,'same image')
 compare_image(map_1, map_2, 'First map vs. Second map')
+compare_image(map_3, map_4, 'Third map vs. Fourth map')
+compare_image(map_5, map_6, 'Fifth map vs Sixth map')
--- green_blue.py
+++ green_blue.py
@@ -3,4 +3,16 @@ import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 from skimage.measure import compare_ssim as ssim
-import cv2
+import cv2 as cv
+
+#load the images
+map_1 = cv.imread('images/509.jpg')
+
+#Resizing the images
+map_1 = cv.resize(map_1, (10000,10000))
+
+#Convert to green blue
+map_1 = cv.cvtColor(map_1, cv.COLOR_BGR2GRAY)
+
+plt.imshow(map_1)
+plt.show()
